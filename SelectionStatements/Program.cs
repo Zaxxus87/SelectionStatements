88160c35ca15014616e65f66b7b632c5a0997122
Jeff Baker <jeffrey1871@gmail.com>
2021-02-23 19:44:46 -0600

    Initial commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Enter an integer between 1 and 1000");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+                Console.WriteLine("too low");
+            else if (userInput > favNumber)
+                Console.WriteLine("too high");
+            else
+                Console.WriteLine("Nevermind");
+
+            
+        }
+    }
+}
